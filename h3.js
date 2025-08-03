e651c69f618305130e9f54c753d79ee33bb0c1e9
gtgdeveloper <daymak3@gmail.com>
2025-08-03 12:22:17 -0400

    Add files via upload

--- h3.js
+++ h3.js
@@ -16,7 +16,8 @@ const GITHUB_REPO = "gx";
 const GITHUB_BRANCH = "main";
 const GITHUB_FILE_PATH = "gtgsol-5-full.json";
 
-const excludedWallets = new Set([
+const excludedWallets = new Set(["F4DACnJRJYhcYswDwHaoLDi9tccwDbiNsA6eyoudTNup","F8gacHyY4APg1ceiUQHVBteHdQ4htTtJxw24wMVTEKWf","9ZHiw3wqtYxrppgAfEbXResxWUasSiVN3jmjQrU9wQYA","2gGSZD8MrrbP1YKJTiQfp5puvVKuMsgAULFAKnF6PSSg","H1G8MR8m7o3xf9Z1wEBGixZ7MAgXGmM1A8L7VcDi16Lb","E5hubdTm1g2PWRLdgjCCVnLhWxc4X1dXNnFgHTM8Wz9c","HyhfwRRSYEL8frDr2kxrmAGgjzJKwyiSbpSALxrwXQfJ","427RBNJvXSDGWSaPFG3YK8U6hR36XiKVZcLYFJnQNCr4","42bCmuhppv9TeQ731mz5xLz5R5NnW8HvPjZPK9H1NJez","BW6ss9hx7EubC7XfwDeCaWoXq8YzEXC3yjGDRMbgEoH8","DzFATJqBCNUnRPUChCxsCtBnUuo7KKVVq3nPLpidnUMP","8pLXCR7fKRaSd7tiriTyCMVb3S1rBSd29mJPU1sFkGuh","ELwyps9sfZ5XmZ4tWgiSGgQbrsKDngsgm9ZxMxHFacM","3mnnj8Ux25bCZ6Mj7VFRDMZoZ9S43fQfjaeNVSqD28Nz","5y8ijbjxfms9GJeyXqTr8iCRqJ5xXWeEHwQpvjK8h5xh","ED8ZpXkydsUZ8YchZDEBjKwdLk4Zqqs8Y8H1XocjhRPN","FAfCCF5A3ZLRS4pBtcPpmpuyNRxmXW6Hj8cfx23XnTbC","7n3UceCNFgSSYCfHVCTeaNAp8dHRv9za6BDvg3XAVcMb","TRSQujxiGfZfM9kQTn4agqr7tfVu1RB1PPvKtCxGvWP","HiyGnXgKuZKFGF6ZoS9Hki4r3mdDmDudGcW5XTQNhJyf","HfGYxdGD123CjYrSyXdWww2az6AMKqmfzFdmDojrynrh","BXEQkRAh5uHP72xqe9efBQRuB2hS3qpuebDp5canP7Pa","5sYuzP7rarmkFCM2m29RmEQSgi4cAgiBBWSvTbg4RhAH","G3fkSwJ7GecBv19g7tT8Rc3YLcyCDiKj9TvdPR5NjfKb","6gKYfBR8Lyt69EhvjKRMuD6hZstVricyoRxyLh3pCTZ6","53YvHR4fsz6trVqjusiyqruhgr1tT8TGSRGPuRAfWMtZ","2tVkWmHTtpht7FRi8n6XkHm8E6G1UyVyUPFQa6946KCk","AzUXusuDcaUfE8VXtFciuPPnzr4RiypwBz5NL6uiydRu","DEk1ZEp7xv8HeP6RHmysx1xJDLeer2qVhawwVjFF364u"]);
+// full exclusion list
   "F4DACnJRJYhcYswDwHaoLDi9tccwDbiNsA6eyoudTNup",
   "F8gacHyY4APg1ceiUQHVBteHdQ4htTtJxw24wMVTEKWf",
   "9ZHiw3wqtYxrppgAfEbXResxWUasSiVN3jmjQrU9wQYA",
@@ -52,7 +53,7 @@ const excludedWallets = new Set([
     const owner = new PublicKey(data.slice(32, 64)).toBase58();
     const amount = Number(data.readBigUInt64LE(64)) / 10 ** 9;
 
-    if (amount >= 20000 && !excludedWallets.has(owner)) {
+    if (amount >= 20000 && amount <= 70000000 && !excludedWallets.has(owner)) {
       totalGTG += amount;
       const sol_equiv = amount * gtg_price_in_sol;
       totalSOL += sol_equiv;
